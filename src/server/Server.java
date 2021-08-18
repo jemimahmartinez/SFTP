b4343e23b8ebb39a83f2f2b4b6b0a0d614aa7ae4
Jemimah Martinez <martinezjemimah@gmail.com>
2021-08-18 13:18:05 +1200

    init commit

---
+++ src/server/Server.java
@@ -0,0 +1,34 @@
+package server;
+
+import java.io.*;
+        import java.net.*;
+
+class Server {
+
+    public static void main(String argv[]) throws Exception
+    {
+        String clientSentence;
+        String capitalizedSentence;
+
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while(true) {
+            // Accept connection from client and initialise data streams
+            Socket connectionSocket = welcomeSocket.accept();
+
+            BufferedReader inFromClient =
+                    new BufferedReader(new
+                            InputStreamReader(connectionSocket.getInputStream()));
+
+            DataOutputStream  outToClient =
+                    new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientSentence = inFromClient.readLine();
+
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
+}
+
